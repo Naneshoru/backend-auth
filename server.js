bfa49a0aa93e324cf43b314dc0f2346a8d120a43
Ricardo Atakiama <ricardo_cssg@hotmail.com>
2025-03-12 13:21:15 -0300

    feat: iniciando aplicação

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.json('Hello world')
+})
+
+app.post('/register', (req, res) => {
+  
+  res.json('Hello world')
+})
+
+app.listen('3000', () => {
+  console.log('Listening on port 3000')
+})
