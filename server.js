945222c5b91a90c0d2c367e77bd76da6c1c18bd4
Ricardo Atakiama <ricardo_cssg@hotmail.com>
2025-03-12 13:44:20 -0300

    chore: mongoose added, logging documents

--- server.js
+++ server.js
@@ -4,6 +4,19 @@ const app = express()
 
 app.use(express.json())
 
+const mongoose = require('mongoose')
+
+const { logDocumentsInCollections } = require('./utils')
+
+main()
+  .then(what => console.log('listening to ', what))
+  .catch(err => console.log(err))
+
+async function main () {
+  await mongoose.connect('mongodb://localhost:27017/test')
+  logDocumentsInCollections()
+}
+
 app.get('/', (req, res) => {
   res.json('Hello world')
 })
